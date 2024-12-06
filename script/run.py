93afe9e9186f54361c95411accf0a05a2cc2c676
SSFC <sortsimilar@gmail.com>
2024-12-06 15:58:20 +0800

    python script/run.py --map c:/gitcloud/LORR24_ssfc/example_problems/random.domain/maps/random-32-32-20.map --plan c:/gitcloud/LORR24_ssfc/cmake-build-release/test.json --grid --aid --tid

--- script/run.py
+++ script/run.py
@@ -73,3 +73,5 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
+# python script/run.py --map c:/gitcloud/LORR24_ssfc/example_problems/random.domain/maps/random-32-32-20.map --plan c:/gitcloud/LORR24_ssfc/cmake-build-release/test.json --grid --aid --tid
